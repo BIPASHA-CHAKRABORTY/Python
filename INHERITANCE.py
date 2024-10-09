d61ad18d62b07d1f4024dc1123cf022012ad6485
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2024-10-09 22:10:11 +0530

    Create INHERITANCE.py

---
+++ INHERITANCE.py
@@ -0,0 +1,19 @@
+#INHERITANCE 
+class A:
+  def __init__(self):
+     self._my="I am proteceted attributes"
+  def _mym(self):
+    print("I am protected method")
+
+class B(A):
+     def __init__(self):
+         super().__init__()
+     def apm(self):
+         print(self._my)
+         self._mym()
+class C(B):
+    def __init__(self):
+       super().__init__(self._my)
+       self._mym()
+obj=C()
+obj.apm()
