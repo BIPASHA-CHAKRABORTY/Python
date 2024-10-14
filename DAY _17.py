f3dc915435e69cd1c77107228f53885bf939bb80
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2024-10-14 23:28:45 +0530

    Create DAY _17.py

---
+++ DAY _17.py
@@ -0,0 +1,25 @@
+'''Operator overloading in python is 1) + symbol is used to strings concatetion and arithmetic addition
+2) * sysmbol is used to multiple a list,string or tuple'''
+
+#USE OF +
+print(10+20)
+print("Python"+ " Programming")
+print([1,2]+[3,4])
+
+#USE OF *
+
+print(" Python"*3)
+print([1,2,3]*4)
+
+
+''Len() func use'''
+my_string="Bipasha Chakraborty"
+my_list=[2,3,4,5,6,7,8,9]
+my_dict={"name":"Namm","Age":3}
+print(len(my_string))
+print(len(my_list))
+print(len(my_dict))
+
+
+
+
