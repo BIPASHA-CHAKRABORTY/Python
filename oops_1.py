273df186862ac5a16a23733aec5a5afe5768b2c6
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2024-10-28 22:14:11 +0530

    Update oops_1.py

--- oops_1.py
+++ oops_1.py
@@ -1,10 +1,13 @@
 class Emp: # creating a class
+    
     def put(self): #indentation is imp inside class
         self.id=int(input("Enter Employee Id:"))
-        self.name=input("Enter Employee Name:")
+        self.name=input("Enter Employee Name:") 
+        
     def display(self):
         print("Employee id:.", self.id)
         print("Employee name:",self.name)
+        
 a=Emp() # calling an object 
 a.put()
 a.display()
